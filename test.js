48ea6782c11a03d25d4c68a47ef0e513ae5077d4
Tyler Gaw <me@tylergaw.com>
2023-09-22 16:03:31 -0400

    Starts CSSStyleValue.parse implementation (#1)

--- test.js
+++ test.js
@@ -60,7 +60,19 @@ const code = Object.keys(units).every(unit => [
 ].every(result => Boolean(result))) && compare(
 	CSS.px(15).add(CSS.rem(10), CSS.em(5)),
 	'calc(15px + 10rem + 5em)'
-) ? 0 : 1;
+) && testCSSStyleValueParse() ? 0 : 1;
+
+function testCSSStyleValueParse() {
+	const expected = { value: 20, unit: 'px'};
+	const result1 = window.CSSStyleValue.parse('width', '20px');
+
+	const conditions = [
+		result1.value === expected.value,
+		result1.unit === expected.unit
+	];
+
+	return conditions.every(condition => Boolean(condition));
+}
 
 console.log(code ? 'Fail...' : 'Pass!')
 
