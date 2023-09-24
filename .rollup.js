cb3f9609f4d3efe5ba2a546cbd0c7d114f897f96
Tyler Gaw <me@tylergaw.com>
2023-09-24 13:46:31 -0400

    Removes Babel and related config/dependencies. (#7)
    * Removes Babel and all related dependencies and configuration as it's no longer needed

--- .rollup.js
+++ .rollup.js
@@ -1,17 +1,7 @@
-import { babel } from '@rollup/plugin-babel';
-
 export default {
-	input: 'index.js',
-	output: [
-		{ file: 'index.umd.js', format: 'umd', name: 'cssTypedOm' },
-		{ file: 'index.es.js', format: 'es' }
-	],
-	plugins: [
-		babel({
-			babelHelpers: 'bundled',
-			presets: [
-				['@babel/preset-env', { modules: false, targets: { node: 18 } }]
-			]
-		})
-	]
+  input: "index.js",
+  output: [
+    { file: "index.umd.js", format: "umd", name: "cssTypedOm" },
+    { file: "index.es.js", format: "es" },
+  ],
 };
